6ed32834fdb1d570139131aac08ab88c3ae250eb
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-11-24 12:08:42 +0800

    fix several bugs

--- cp1404practicals/prac_01/broken_score.py
+++ cp1404practicals/prac_01/broken_score.py
@@ -3,17 +3,17 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-# TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
+def main():
+    score = float(input("Enter score: "))
+    if 100 < score < 0:
         print("Invalid score")
-    if score > 50:
+    elif 90 > score >= 50:
         print("Passable")
-    if score > 90:
-    print("Excellent")
-if score < 50:
-    print("Bad")
+    elif score >= 90:
+        print("Excellent")
+    else:
+        print("Bad")
+
+
+main()
