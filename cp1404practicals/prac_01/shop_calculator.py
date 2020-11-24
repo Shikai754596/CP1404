aab52ac87de7ed8a25bc5a810ce116c5389350c8
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-11-24 12:52:01 +0800

    Initial commit

---
+++ cp1404practicals/prac_01/shop_calculator.py
@@ -0,0 +1,12 @@
+def main():
+    num_item = int(input("Number of items: "))
+    while num_item < 0:
+        print("Invalid number of items!")
+    total_price = 0
+    for i in range(num_item):
+        total_price += float(input("Price of item: "))
+    if total_price > 100:
+        total_price = total_price * 0.9
+    print("Total price for 3 items is ${}".format(total_price))
+
+main()
