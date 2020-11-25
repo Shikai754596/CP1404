b5ed23e26146bc71b686113ea555f600c2cafc3c
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-11-25 17:28:53 +0800

    Complete the part to add up all the numbers

--- cp1404practicals/prac_02/files.py
+++ cp1404practicals/prac_02/files.py
@@ -1,15 +1,22 @@
 def main():
-
     name = input("Please enter your name: ")
     name_input_file = open("name.txt", "w")
     print(name, file=name_input_file)
     name_input_file.close()
+
     name_output_file = open("name.txt", "r")
     print("Your name is {}".format(name_output_file.read()))
+
     number_file = open("numbers.txt", "r")
     number = 0
     for i in range(2):
         number += int(number_file.readline())
+    print(number)
+
+    number_file = open("numbers.txt", "r")
+    number = 0
+    for line_number in number_file:
+        number += int(line_number)
     print(number)
 
 
