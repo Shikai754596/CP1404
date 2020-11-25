e4d083ddf0870be46b44c4a7a50381ad71808ed9
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-11-25 17:16:32 +0800

    Complete the part to input and output name.txt

--- cp1404practicals/prac_02/files.py
+++ cp1404practicals/prac_02/files.py
@@ -1,7 +1,11 @@
 def main():
 
-    
-
+    name = input("Please enter your name: ")
+    name_input_file = open("name.txt", "w")
+    print(name, file=name_input_file)
+    name_input_file.close()
+    name_output_file = open("name.txt", "r")
+    print("Your name is {}".format(name_output_file.read()))
     number_file = open("numbers.txt", "r")
     number = 0
     for i in range(2):
