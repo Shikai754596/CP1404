25e28b12b4a28b6393a93e7823ba169c36ec2d8c
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-11-25 17:58:43 +0800

    Complete all the TODO

--- cp1404practicals/prac_02/password_checker.py
+++ cp1404practicals/prac_02/password_checker.py
@@ -32,21 +32,24 @@ def is_valid_password(password):
     # TODO: if length is wrong, return False
     if MAX_LENGTH < len(password) or len(password) < MIN_LENGTH:
         return False
-
     count_lower = 0
     count_upper = 0
     count_digit = 0
     count_special = 0
     for char in password:
-        
-
-
-    # TODO: if any of the 'normal' counts are zero, return False
-
-    # TODO: if special characters are required, then check the count of those
-    # and return False if it's zero
-
-    # if we get here (without returning False), then the password must be valid
+        if char.islower() is True:
+            count_lower += 1
+        elif char.isupper() is True:
+            count_upper += 1
+        elif char.isdigit() is True:
+            count_digit += 1
+        elif char in SPECIAL_CHARACTERS:
+            count_special += 1
+    if count_digit == 0 or count_upper == 0 or count_lower == 0:
+        return False
+    if SPECIAL_CHARS_REQUIRED is not False:
+        if count_special == 0:
+            return False
     return True
 
 
