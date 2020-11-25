740b25ffe99648317e3eac0ac4911a1a9b8b1eeb
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-11-25 14:22:20 +0800

    add codes to produce a random number between 1 and 100

--- cp1404practicals/prac_02/randoms.py
+++ cp1404practicals/prac_02/randoms.py
@@ -1,17 +1,25 @@
 # What did you see on line 1?
 # I got a random integer between 5 and 20
+
 # What was the smallest number you could have seen, what was the largest?
 # The smallest: 5     The largest: 20
 
-
 # What did you see on line 2?
 # I got a random number from 3, 5, 7, 9
+
 # What was the smallest number you could have seen, what was the largest?
 # The smallest: 3     The largest: 9
+
 # Could line 2 have produced a 4?
 # No
 
 # What did you see on line 3?
 # I got a random float number between 2.5 and 5.5
+
 # What was the smallest number you could have seen, what was the largest?
 # The smallest: 2.5     The largest: 5.5
+
+
+import random
+
+print(random.randint(1, 100))
