fb7ada353c9f4cfc4dcc2fcacb2b3bf221761568
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-12-07 22:40:47 +0800

    Add numbers_calculation function to output numbers as requested

--- cp1404practicals/prac_04/list_exercises.py
+++ cp1404practicals/prac_04/list_exercises.py
@@ -1,12 +1,28 @@
 def main():
     numbers = get_numbers(5)
+    numbers_calculation(numbers)
 
 
 def get_numbers(times):
+    """Get several numbers from user and output a list of these numbers"""
     number_list = []
     for i in range(times):
         number_list += [int(input("Number: "))]
     return number_list
+
+
+def numbers_calculation(number_list):
+    """Calculate and print numbers from the list as requested"""
+    first_number = number_list[0]
+    last_number = number_list[-1]
+    smallest_number = min(number_list)
+    largest_number = max(number_list)
+    average_number = sum(number_list) / len(number_list)
+    print("The first number is {}".format(first_number))
+    print("The last number is {}".format(last_number))
+    print("The smallest number is {}".format(smallest_number))
+    print("The largest number is {}".format(largest_number))
+    print("The average of the numbers is {}".format(average_number))
 
 
 main()
