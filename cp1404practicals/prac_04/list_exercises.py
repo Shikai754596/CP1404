5053f989f0041013e7e88430dcc883753af0202d
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-12-07 22:13:16 +0800

    Add function to get a list of several numbers

--- cp1404practicals/prac_04/list_exercises.py
+++ cp1404practicals/prac_04/list_exercises.py
@@ -1,1 +1,12 @@
+def main():
+    numbers = get_numbers(5)
 
+
+def get_numbers(times):
+    number_list = []
+    for i in range(times):
+        number_list += [int(input("Number: "))]
+    return number_list
+
+
+main()
