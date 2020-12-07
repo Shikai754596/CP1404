5d0730bae8d4b4c7897dce62a772a0b859e1df4c
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-12-07 20:39:32 +0800

    Achieve the following 4 requests

--- cp1404practicals/prac_04/lists_warmup.py
+++ cp1404practicals/prac_04/lists_warmup.py
@@ -9,3 +9,9 @@ print(7 in numbers)  # False
 print("3" in numbers)  # False
 print(numbers + [6, 5, 3])  # [3, 1, 4, 5, 9, 2, 6, 5, 3]
 
+print()
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers)
+print(numbers[2:])
+print(9 in numbers)
