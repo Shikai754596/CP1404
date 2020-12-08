b668108bd46d2ee77de200796c80ce0c9918fbdf
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-12-08 14:59:26 +0800

    Refactor the structure of loops and format to meet requirements

--- cp1404practicals/prac_04/quick_picks.py
+++ cp1404practicals/prac_04/quick_picks.py
@@ -4,17 +4,14 @@ import random
 def main():
     lines = int(input("How many quick picks? "))
     CONSTANTS = []
-    number_lines = []
     for i in range(lines):
-        for t in range(5):
+        number_lines = []
+        for t in range(6):
             number_lines += [random.randint(1, 45)]
         CONSTANTS.append(number_lines)
         for number in CONSTANTS[i]:
-            print(number, end=" ")
+            print("{:2}".format(number), end=" ")
         print()
-
-
-
 
 
 main()
