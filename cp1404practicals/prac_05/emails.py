e106ad8d8c46c5c5836bdc0753242ee220584ef7
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-12-16 04:42:38 +0800

    ask user if name is correct and add emails and names into dictionary

--- cp1404practicals/prac_05/emails.py
+++ cp1404practicals/prac_05/emails.py
@@ -1,8 +1,19 @@
 def main():
     email = input("Email: ")
+    email_dict = {}
     while email.isspace() is False or email != "":
         name = email.split("@")[0]
-        answer = input("Is your name {}? (y/n)".format(name))
+        answer = input("Is your name {}? (Y/N)".format(name)).upper()
+        if answer == "Y" or "YES":
+            email_dict[name] = email
+        elif answer == "N" or "NO":
+            name = input("Name: ")
+            email_dict[name] = email
+        email = input("Email: ")
+
+    for dict_name, dict_email in email_dict.items():
+        print("{} ({})")
+
 
 
 main()
