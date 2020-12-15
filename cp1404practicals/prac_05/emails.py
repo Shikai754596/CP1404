ff794f937a21d95b6713a228242b8bdf943e4f46
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-12-16 04:58:11 +0800

    print the dictionary of names and emails

--- cp1404practicals/prac_05/emails.py
+++ cp1404practicals/prac_05/emails.py
@@ -1,19 +1,22 @@
 def main():
     email = input("Email: ")
     email_dict = {}
-    while email.isspace() is False or email != "":
+    while email != "" and email.isspace() is False:
         name = email.split("@")[0]
-        answer = input("Is your name {}? (Y/N)".format(name)).upper()
+        name = " ".join(name.split(".")).title()
+        answer = input("Is your name {}? (Y/N) ".format(name)).upper()
+        while answer not in "Y N YES NO":
+            answer = input("Please choose (Y/N) ".format(name)).upper()
         if answer == "Y" or "YES":
             email_dict[name] = email
         elif answer == "N" or "NO":
+            print("pa")
             name = input("Name: ")
             email_dict[name] = email
         email = input("Email: ")
 
     for dict_name, dict_email in email_dict.items():
-        print("{} ({})")
-
+        print("{} ({})".format(dict_name, dict_email))
 
 
 main()
