0ea93667c4570ca9784139949b1241fd8177a5ab
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-12-23 20:24:19 +0800

    Complete test

--- cp1404practicals/prac_06/guitar_test.py
+++ cp1404practicals/prac_06/guitar_test.py
@@ -1,7 +1,14 @@
 from cp1404practicals.prac_06.guitar import Guitar
 
+
 def main():
     gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
     another = Guitar("Another Guitar", 2013)
+
+    print("{} get_age() - Excepted 98. Got {}".format(gibson.name, gibson.get_age()))
+    print("{} get_age() - Excepted 7. Got {}".format(another.name, another.get_age()))
+    print("{} is_vintage() - Expected True. Got {}".format(another.name, another.is_vintage()))
+    print("{} is_vintage() - Expected True. Got {}".format(another.name, another.is_vintage()))
+
 
 main()
