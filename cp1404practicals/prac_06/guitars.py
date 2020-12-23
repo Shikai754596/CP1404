5f75298755c0c0c1e4e019963c1c16c7aa512ac6
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-12-23 21:03:24 +0800

    print guitars' information one by one from list in correct format

--- cp1404practicals/prac_06/guitars.py
+++ cp1404practicals/prac_06/guitars.py
@@ -5,12 +5,20 @@ def main():
     guitars = [Guitar("Gibson L-5 CES", 1922, 16035.40), Guitar("Line 6 JTV-59", 2010, 1512.9)]
     name = input("Name: ")
     while name != "" and name.isspace() is False:
-        year = input("Year: ")
-        cost = input("Cost: ")
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
         guitar = Guitar(name, year, cost)
         guitars.append(guitar)
         print(guitar)
         print()
         name = input("Name: ")
+    print("\n... snip ...\n")
+    print("These are my guitars: ")
+    for i, guitar in enumerate(guitars):
+        if guitar.is_vintage() is False:
+            vintage_string = ""
+        else:
+            vintage_string = "(vintage)"
+        print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost, vintage_string))
 
 main()
