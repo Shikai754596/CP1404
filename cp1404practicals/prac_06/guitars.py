1664e271cee4f545b3bb522f9db2b51d93953e51
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-12-23 20:44:26 +0800

    Add loops and list

--- cp1404practicals/prac_06/guitars.py
+++ cp1404practicals/prac_06/guitars.py
@@ -2,10 +2,15 @@ from cp1404practicals.prac_06.guitar import Guitar
 
 def main():
     print("My guitars!")
+    guitars = [Guitar("Gibson L-5 CES", 1922, 16035.40), Guitar("Line 6 JTV-59", 2010, 1512.9)]
     name = input("Name: ")
-    year = input("Year: ")
-    cost = input("Cost: ")
-    guitar = Guitar(name, year, cost)
-    print(guitar)
+    while name != "" and name.isspace() is False:
+        year = input("Year: ")
+        cost = input("Cost: ")
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(guitar)
+        print()
+        name = input("Name: ")
 
 main()
