5b02a67767510c035349c323548d25df07f95e08
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-12-23 19:01:34 +0800

    Add 4 fields for __init__

--- cp1404practicals/prac_06/programming_language.py
+++ cp1404practicals/prac_06/programming_language.py
@@ -1,4 +1,10 @@
 class ProgrammingLanguage:
 
-    def __init__(self):
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
         
