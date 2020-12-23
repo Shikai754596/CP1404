4acabacb78942f0a80c6e30774a5cd6332151b1b
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-12-23 19:58:25 +0800

    Add __str__, get_age(), is_vintage() method as requested

--- cp1404practicals/prac_06/guitar.py
+++ cp1404practicals/prac_06/guitar.py
@@ -3,4 +3,13 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
-        
+
+    def __str__(self):
+        return "{} ({}) : ${}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        age = 2020 - self.year
+        return age
+
+    def is_vintage(self):
+        return self.get_age() >= 50
