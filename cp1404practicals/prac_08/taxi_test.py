81879ed38cff11645ff8198849a5ba82e3a189a9
Shikai754596 <shikai.huang@my.jcu.edu.au>
2021-01-13 03:49:34 +0800

    delete the price_per_km value

--- cp1404practicals/prac_08/taxi_test.py
+++ cp1404practicals/prac_08/taxi_test.py
@@ -1,7 +1,7 @@
 from cp1404practicals.prac_08.taxi import Taxi
 
 def main():
-    new_taxi = Taxi(100, "Prius 1", 1.23)
+    new_taxi = Taxi(100, "Prius 1")
     new_taxi.drive(40)
     print(new_taxi)
     print("Current fare is {}".format(new_taxi.get_fare()))
