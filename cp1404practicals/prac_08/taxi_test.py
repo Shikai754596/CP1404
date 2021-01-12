984d08b582443e58df98ed792560e94e417df29e
Shikai754596 <shikai.huang@my.jcu.edu.au>
2021-01-13 03:36:21 +0800

    print current fare

--- cp1404practicals/prac_08/taxi_test.py
+++ cp1404practicals/prac_08/taxi_test.py
@@ -1,8 +1,10 @@
 from cp1404practicals.prac_08.taxi import Taxi
 
 def main():
-    new_taxi = Taxi("Prius 1", 100, 1.23)
+    new_taxi = Taxi(100, "Prius 1", 1.23)
     new_taxi.drive(40)
     print(new_taxi)
+    print("Current fare is {}".format(new_taxi.get_fare()))
+
 
 main()
