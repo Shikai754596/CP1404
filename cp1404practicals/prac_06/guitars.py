72d35731290b43a76609af97d8cb83cafb0dec71
Shikai754596 <shikai.huang@my.jcu.edu.au>
2021-01-13 00:30:50 +0800

    Change button 2 to a text input field

--- cp1404practicals/prac_06/guitars.py
+++ cp1404practicals/prac_06/guitars.py
@@ -1,5 +1,6 @@
 from cp1404practicals.prac_06.guitar import Guitar
 
+
 def main():
     print("My guitars!")
     guitars = [Guitar("Gibson L-5 CES", 1922, 16035.40), Guitar("Line 6 JTV-59", 2010, 1512.9)]
@@ -19,6 +20,8 @@ def main():
             vintage_string = ""
         else:
             vintage_string = "(vintage)"
-        print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost, vintage_string))
+        print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
+                                                                  vintage_string))
+
 
 main()
