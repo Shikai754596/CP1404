04bed4d1b11d1f4f75df099deac14e1bd93bd006
Shikai754596 <shikai.huang@my.jcu.edu.au>
2021-01-13 05:03:58 +0800

    Reformat the file

--- cp1404practicals/prac_08/silver_service_taxi_test.py
+++ cp1404practicals/prac_08/silver_service_taxi_test.py
@@ -1,11 +1,11 @@
 from cp1404practicals.prac_08.silver_service_taxi import SilverServiceTaxi
 
+
 def main():
     silver_taxi = SilverServiceTaxi(100, "Hummer", 2)
     silver_taxi.drive(18)
     print(silver_taxi)
-    print(silver_taxi.get_fare())
-
+    print("The fare is ${}".format(silver_taxi.get_fare()))
 
 
 main()
