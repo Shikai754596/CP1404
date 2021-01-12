30d6c5dfd4246a94c006c47f3504f8943365ce98
Shikai754596 <shikai.huang@my.jcu.edu.au>
2021-01-13 02:51:12 +0800

    Delete window size and add creat_widgets method

--- cp1404practicals/prac_07/dynamic_labels.py
+++ cp1404practicals/prac_07/dynamic_labels.py
@@ -1,25 +1,26 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.core.window import Window
 from kivy.uix.button import Button
 from kivy.properties import StringProperty
 
 
 class DynamicLabelsApp(App):
     status_text = StringProperty()
-    
+
     def __init__(self, **kwargs):
         """Construct main app."""
         super().__init__(**kwargs)
         self.name_list = ["Bob Brown", "Cat Cyan", "Oren Ochre", "John Wick", "Jack Watson"]
 
     def build(self):
-        Window.size = (600, 200)
         self.title = "Dynamic Labels"
         self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_widgets()
         return self.root
 
     def create_widgets(self):
         for name in self.name_list:
             temp_button = Button(text=name, id=name)
             self.root.ids.entries_box.add_widget(temp_button)
+
+DynamicLabelsApp().run()
