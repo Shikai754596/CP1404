4584030810fd574aa6fce28e7e20a97c800c913d
Shikai754596 <shikai.huang@my.jcu.edu.au>
2021-01-13 04:22:36 +0800

    Add random number into test codes

--- cp1404practicals/prac_08/unreliable_car_test.py
+++ cp1404practicals/prac_08/unreliable_car_test.py
@@ -1,10 +1,12 @@
 from cp1404practicals.prac_08.unreliable_car import UnreliableCar
+import random
 
 
 def main():
-    new_car = UnreliableCar(100, "New Car", 100)
-    new_car.drive(0)
-    print(new_car)
+    new_car = UnreliableCar(100, "New Car", 50)
+    while new_car.fuel != 0:
+        new_car.drive(random.randint(0, 50))
+        print(new_car)
 
 
 
