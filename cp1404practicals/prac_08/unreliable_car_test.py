cfbc798493f068d9fa5388b560b99b536d97c5ad
Shikai754596 <shikai.huang@my.jcu.edu.au>
2021-01-13 04:18:53 +0800

    Add some simple test codes

---
+++ cp1404practicals/prac_08/unreliable_car_test.py
@@ -0,0 +1,11 @@
+from cp1404practicals.prac_08.unreliable_car import UnreliableCar
+
+
+def main():
+    new_car = UnreliableCar(100, "New Car", 100)
+    new_car.drive(0)
+    print(new_car)
+
+
+
+main()
